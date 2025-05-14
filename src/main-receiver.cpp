164feb852a6852105d08406e8fb5df079d3d33ac
Lars @ Gallery <hagemann.l+gallery@outlook.com>
2025-05-14 17:42:42 +0100

    feat: test setup for transmitter

--- src/main-receiver.cpp
+++ src/main-receiver.cpp
@@ -1,7 +1,15 @@
 #include <cstdio>
 #include <openhd_global_constants.hpp>
+#include <openhd_profile.h>
+#include <ohd_interface.h>
 
 int main() {
-  std::printf("OpenHD Version: %s!\n", openhd::get_ohd_version_as_string().c_str());
-  std::printf("Hello, World from receiver!\n");
+  const auto profile = DProfile::discover(false);
+  write_profile_manifest(profile);
+  
+  auto ohdInterface = std::make_shared<OHDInterface>(profile);
+
+  OHDInterface::generate_keys_from_pw_if_exists_and_delete();
+
+
 }
--- src/main-transmitter.cpp
+++ src/main-transmitter.cpp
@@ -1,7 +1,54 @@
-#include <cstdio>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <csignal>
+
 #include <openhd_global_constants.hpp>
+#include <openhd_profile.h>
+#include <ohd_interface.h>
+#include <gstaudiostream.h>
+
+static auto quit = false;
 
 int main() {
-  std::printf("OpenHD Version: %s!\n", openhd::get_ohd_version_as_string().c_str());
-  std::printf("Hello, World from transmitter!\n");
+  const auto profile = DProfile::discover(true);
+  write_profile_manifest(profile);
+  
+  auto ohdInterface = std::make_shared<OHDInterface>(profile);
+  auto link_handle = ohdInterface->get_link_handle();
+  if (!link_handle) {
+    std::cerr << "Failed to get link handle" << std::endl;
+    return -1;
+  }
+
+  OHDInterface::generate_keys_from_pw_if_exists_and_delete();
+
+  auto gstAudioStream = std::make_shared<GstAudioStream>();
+  gstAudioStream->set_link_cb(
+      [&link_handle](const openhd::AudioPacket& audioPacket) {
+        std::cout << "Got audio packet of size: " << audioPacket.data->size()
+                  << '\n';
+        link_handle->transmit_audio_data(audioPacket);
+      });
+  gstAudioStream->start_looping();
+
+  signal(SIGTERM, [](int sig) {
+    std::cerr << "Got SIGTERM, exiting\n";
+    quit = true;
+  });
+  signal(SIGQUIT, [](int sig) {
+    std::cerr << "Got SIGQUIT, exiting\n";
+    quit = true;
+  });
+
+
+  while (!quit) {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
+
+  gstAudioStream->stop_looping();
+  gstAudioStream.reset();
+  ohdInterface.reset();
+
+  std::cout << "Exiting main loop\n";
 }
